ca721b1f0c419576cc5417b7567a8f12c2e48850
Moldashbek Sultankul <sultankulmaksatuly@gmail.com>
2023-09-22 00:11:33 +0600

    thryt

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,17 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
-
+        int x = 0;
+        while (x <= printToInclusive){
+            System.out.println(getFactorial(x));
+            x++;
+        }
+    }
+    public static int getFactorial(int f) {
+        int result = 1;
+        for (int i = 1; i <= f; i++) {
+            result = result * i;
+        }
+        return result;
     }
 }
